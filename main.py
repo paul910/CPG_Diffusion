9fe0794831dc142343ca709b5040afd2d75d4481
paul910 <paul.ehrhard@web.de>
2023-06-22 18:05:39 +0200

    adj model size

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class Diffusion:
         self.test_loader = DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
 
         self.model_path = model_path
-        self.model_depth = 5
+        self.model_depth = 4
         self.model_mult_factor = 3
         self.time_embedding_size = 32
         self.model = GDNN(self.num_node_features, self.time_embedding_size, self.model_depth, self.model_mult_factor).to(self.device)
